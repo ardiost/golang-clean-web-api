bcda5a81929e74e2592c999b81dabf3b1add0dfa
Ardalan Ostadpour <a.ostadpour@pars.local>
2024-11-09 17:20:37 +0330

    add config to project

--- src/api/api.go
+++ src/api/api.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/ardiost/golang-clean-web-api/api/routers"
+	"github.com/ardiost/golang-clean-web-api/config"
 	"github.com/gin-gonic/gin"
 )
 
 func InitServer() {
+	cfg := config.GetConfig()
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 	v1 := r.Group("/api/v1/")
@@ -13,5 +17,5 @@ func InitServer() {
 		health := v1.Group("/health")
 		routers.Health(health)
 	}
-	r.Run(":5005")
+	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
 }
